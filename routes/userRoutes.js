36798992b0d85a7371939b0a9229002322cb3bd8
mohaelmi <moha.abdi1771@gmail.com>
2023-08-16 18:18:04 -0500

    fix routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,45 @@
 
 const express = require("express");
 const router = express.Router();
+const usersQueries = require("../db/queries/users");
 
+// Getting all users
 router.get("/", (req, res) => {
-  res.render("users");
+  usersQueries
+    .getUsers()
+    .then((users) => {
+      res.json({ users });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
 
+// Get user by its id
+router.get("/:id", (req, res) => {
+  const userId = req.params.id;
+  usersQueries
+    .getUserById(userId)
+    .then((users) => {
+      res.json({ users });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+// Create New User
+router.post("/new", (req, res) => {
+  const user = JSON.parse(req.body);
+  // const user = req.body;
+  console.log("user: ", user);
+  usersQueries
+    .addNewUser(user)
+    .then((user) => {
+      res.json({ user });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
 module.exports = router;
