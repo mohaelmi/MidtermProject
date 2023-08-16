b2a1042a46377a1f7b215f358026caf2e79b4597
sfia-o <sofia.vl.oliveira@gmail.com>
2023-08-16 23:08:27 +0000

    Initial commit

---
+++ public/scripts/new_listing.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  /**
+   * Create new listing
+   */
+
+  const newListing = function(bikeData) {
+
+  };
+
+  /**
+   * Add Photos to Database
+   */
+
+  const addPhotos = function(photoURL) {
+
+  };
+})
---
+++ public/scripts/product.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  /**
+   * Render Product Info
+   */
+
+  const loadProduct = function(bikeData) {
+
+  };
+
+  /**
+   * Send SMS to seller
+   */
+
+  const messageSeller = function(message) {
+
+  };
+})
