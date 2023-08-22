ab3877a93f445e5ae89d76d2e5616d3b55553a65
Janay Ma <janay.ma7@gmail.com>
2023-08-22 01:25:04 -0700

    fix linting errors

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -27,10 +27,10 @@ $(document).ready(function() {
   $('.my-listings').on('click', viewMyListings);
 
   $('.all-listings').on('click', () => {
-    console.log('all bikes clicked')
+    console.log('all bikes clicked');
     $('.listing-container').empty();
     loadItems();
-  } )
+  });
 
   /*---------- admin listing listeners -----------*/
   //must implement
@@ -54,13 +54,13 @@ $(document).ready(function() {
  * Load All Items/Bikes onto landing page
  */
 const loadItems = function() {
-   $.get('/api/items')
-     .then( data => {
-       //console.log('it worked!')
-       renderItems(data.items)
-     })
-     .catch(err => console.log(err.message));
- }
+  $.get('/api/items')
+    .then(data => {
+      //console.log('it worked!')
+      renderItems(data.items);
+    })
+    .catch(err => console.log(err.message));
+};
 
 
 /*---------- navbar buttons ----------*/
@@ -72,35 +72,35 @@ const showBikeForm = function() {
 
   button.addEventListener("click", function() {
     if (dropdownForm.style.display === "none") {
-      dropdownForm.style.display = "flex"
+      dropdownForm.style.display = "flex";
     } else {
-      dropdownForm.style.display = "none"
+      dropdownForm.style.display = "none";
     }
   });
-}
+};
 
 
 //searchbar listener
-const searchBarButton = function () {
+const searchBarButton = function() {
   $('.search-button').on('click', (e) => {
     e.preventDefault();
-    const minPrice = $('#min-price').val()
-    const maxPrice = $('#max-price').val()
+    const minPrice = $('#min-price').val();
+    const maxPrice = $('#max-price').val();
     const data = {
       minPrice,
       maxPrice
-    }
+    };
     //console.log(data);
     $.post("/search", data, (data) => {
-      $('.listing-container').empty()
-      renderItems(data.data)
+      $('.listing-container').empty();
+      renderItems(data.data);
       console.log(data);
 
     })
-    .catch(err => err.message)
-   // alert('Seach bar clicked!')
-  })
-}
+      .catch(err => err.message);
+    // alert('Seach bar clicked!')
+  });
+};
 
 
 /*---------- new bike form buttons ----------*/
@@ -109,11 +109,11 @@ const searchBarButton = function () {
 //associate image with bike you are posting
 const addImageButton = function() {
   alert('you should add an image!');
-}
+};
 
 
 //post new bike button listener
-const postBikeButton = function () {
+const postBikeButton = function() {
 
   const $button = $('.post-button');
   $button.on('click', function(e) {
@@ -133,30 +133,30 @@ const postBikeButton = function () {
       description,
       condition: 'New',
       status: 'Available'
-    }
+    };
     $.post("/api/items", item, (data) => {
       $('.listing-container').empty();
-      loadItems()
+      loadItems();
       const dropdownForm = document.querySelector(".dropdown-form");
-      dropdownForm.style.display = "none"
+      dropdownForm.style.display = "none";
       console.log(data);
 
     })
-    .catch(err => err.message)
-  })
+      .catch(err => err.message);
+  });
 
-}
+};
 
 /*---------- toggle bar buttons ----------*/
 
 //needs new favourites route to return the whole item, not just id
 //view all favourite items for certain id:
-const viewFavourites = function () {
+const viewFavourites = function() {
   $.get('api/favourites/')
     .then(data => {
       console.log(data.items);
-    })
-}
+    });
+};
 
 
 /**
@@ -167,17 +167,17 @@ const loadFavourites = function(items) {
   const favouritesButton = document.querySelector(".favourites");
 
   favouritesButton.addEventListener("click", function() {
-    console.log(items)
+    console.log(items);
 
     $.get('/api/items')
-    .then( data => {
+      .then(data => {
 
-        renderItems(data.items)
-    })
+        renderItems(data.items);
+      });
 
   });
 
-  };
+};
 
 
 //view only your listings
@@ -187,8 +187,8 @@ const viewMyListings = function() {
       $('.listing-container').empty();  //get rid of current shown listings
       renderItems(data.items, true);
     })
-    .catch(err => console.log('error', err))
-}
+    .catch(err => console.log('error', err));
+};
 /*---------- listing buttons ----------*/
 
 // adds/deletes item to favourites if the button is clicked.
@@ -207,23 +207,23 @@ const toggleFavourite = function() {
         console.log('deleting item from favourites!');
         $(this).toggleClass('red', item.favourite);
       }
-    })
+    });
     return;
   }
 
   //otherwise, add item to favourites!
   $.post(`/api/favourites/${item.id}`)
-  .then(res => {
-    console.log('posting!')
-    $(this).toggleClass('red', item.favourite);
-  })
-}
+    .then(res => {
+      console.log('posting!');
+      $(this).toggleClass('red', item.favourite);
+    });
+};
 
 //brings up form to message seller regarding bike. need to implement twilio api call
 const messageSeller = function() {
- $('#message-popup').popup()
+  $('#message-popup').popup();
   alert('message the seller!');
-}
+};
 
 /**
  * Delete Bike
@@ -239,7 +239,7 @@ const deleteBike = function(item) {
 //creates a pop up div to message seller. need to create html to render.
 const renderUserMessage = function(data) {
   return 'hello world!';
-}
+};
 
 //renders a listing for an item for sale
 const createItemElement = function(data, isOwner) {
@@ -318,8 +318,8 @@ const renderItems = function(items, isOwner) {
   items.reverse().forEach((item, i) => {
     const element = createItemElement(item, isOwner);
     container.prepend(element);
-  })
-}
+  });
+};
 
 
 
