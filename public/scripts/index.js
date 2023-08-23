12c04a832836888c044fe53ea2f2c7af63889c43
sfia-o <sofia.vl.oliveira@gmail.com>
2023-08-22 17:12:23 -0700

    Implemented TimeAgo

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -284,7 +284,7 @@ const createItemElement = function(data, isOwner) {
   const itemCondition = data.condition;
   const itemDescription = data.description;
   const itemSize = data.size;
-  const postDate = data.created_at;
+  const postDate = timeago.format(data.created_at);
 
   let element;
 
