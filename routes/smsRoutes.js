0a3461e91230c65020f749110aa2edad3206e8e6
mohaelmi <moha.abdi1771@gmail.com>
2023-08-24 18:25:23 -0500

    grap user info

--- routes/smsRoutes.js
+++ routes/smsRoutes.js
@@ -5,24 +5,25 @@ const userAdminQueries = require("../db/queries/users");
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+
 const twilio = require('twilio')(accountSid, authToken);
 
 
+const sendMessage = function(sellerPhone, message) {
+  
 
-const sendMessage = function(sellerPhone, buyerPhone, message) {
-
-
-  return twilio.messages
+     twilio.messages
       .create({
           body: message, // 'Hey this is a test message.',  // req.body.message
-          from: buyerPhone, // '+18643652977' //req.body.number // after you sign up you will have a virtual number from twilio
+          from: '+18643652977', //buyerPhone, // '+18643652977' //req.body.number // after you sign up you will have a virtual number from twilio
           to: sellerPhone // req.body.userNumber  // add your number to test 
       })
       .then((message) => {
-        return message
+        return message.accountSid
       })
       .catch((err) => {
-        console.log(err.message);
+        console.log(err);
+        return err
       });
 
 }
@@ -30,22 +31,17 @@ const sendMessage = function(sellerPhone, buyerPhone, message) {
 // // Send message route
 router.post("/", (req, res) => {
   console.log("in sms route!");
-  const userId = req.body.seller_id;
-  const buyerPhone = req.body.cell 
-  const message = req.body.message 
-
-  if (!userId) {
-    return res.send({ error: "Please Login" });
-  }
+  const userId = req.body['user-id'];
+  const buyerPhone = req.body['user-cell']; 
+  const message = req.body['user-message']; 
+ 
 
   userAdminQueries
     .getUserById(userId)
     .then((user) => {
       const sellerPhone = user.phone;
-    sendMessage(sellerPhone, buyerPhone, message)
-    .then((data) => {
-      res.json({ data });
-    })
+      console.log('seller phone: ', sellerPhone, 'buyer: ', buyerPhone, 'message: ',  message );
+    sendMessage(sellerPhone, message)
     })
     .catch((err) => {
       res.status(500).json({ error: err.message });
